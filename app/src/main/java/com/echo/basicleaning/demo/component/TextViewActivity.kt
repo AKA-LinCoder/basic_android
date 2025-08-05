c1d4e940b01faab3d313d15c272e2d7cf6c22dcb
lin <2423860442@qq.com>
2025-08-05 16:26:58 +0800

    textview跑马灯

--- app/src/main/java/com/echo/basicleaning/demo/component/TextViewActivity.kt
+++ app/src/main/java/com/echo/basicleaning/demo/component/TextViewActivity.kt
@@ -1,5 +1,6 @@
 package com.echo.basicleaning.demo.component
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -7,11 +8,16 @@ import com.echo.basicleaning.R
 
 class TextViewActivity : AppCompatActivity() {
     lateinit var tv1:TextView
+    lateinit var tv2:TextView
+//    lateinit var binding: ActivityTextViewActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_view)
 
         tv1 = findViewById(R.id.tv1)
+        tv2 = findViewById(R.id.tv2)
         tv1.text = "你好啊"
+        tv2.isSelected = true
+
     }
 }
